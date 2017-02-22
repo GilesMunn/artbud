18f1fbe6579b84d93377160705b120d9f0716f3b
GilesGlasgow <2227780m@student.gla.ac.uk>
2017-02-22 16:56:48 +0000

    Added population script, categories, pages

--- artbud_project/artbud/views.py
+++ artbud_project/artbud/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from artbud.models import Category
 
 def index(request):
-	return render(request,'artbud/index.html')
+	category_list = Category.objects.order_by('-likes')[:5]
+	context_dict = {'categories': category_list}
+	return render(request, 'artbud/index.html', context_dict)
+
 
 def about(request):
 	return render(request,'artbud/about.html')
---
+++ artbud_project/populate_artbud.py
@@ -0,0 +1,75 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','artbud_project.settings')
+
+import django
+django.setup()
+
+from artbud.models import Category, Page
+
+def populate():
+	
+	abstract_pages = [
+		{'title': 'Ideas',
+		'url':'http://inspirationseek.com/wp-content/uploads/2014/06/Abstract-Painting-Ideas.jpg', 'views': 12},
+		{'title': 'Nature',
+		'url':'http://inspirationseek.com/wp-content/uploads/2014/06/Abstract-Painting-Nature.jpg', 'views': 34},
+		{'title': 'Flower Nature',
+		'url':'http://inspirationseek.com/wp-content/uploads/2014/06/Abstract-Painting-Flower-Nature.jpg', 'views': 15} ]
+		
+	still_life_pages = [
+		{'title': 'Bas Meeuws',
+		'url':'http://cdn.mos.cms.futurecdn.net/rmnxHaXkhmZjncyzMahdJU-650-80.jpg', 'views': 24 },
+		{'title': 'Elena K',
+		'url':'http://cdn.mos.cms.futurecdn.net/fff6c24beaf409b2d24e479a414374a9-650-80.jpg', 'views': 86 },
+		{'title': 'Joan Gamell',
+		'url':'http://cdn.mos.cms.futurecdn.net/6133a6789a940f061a2a4eaf898ad67d-650-80.jpg', 'views': 201 } ]
+		
+	impressionist_pages = [
+		{'title': 'Les raboteurs de parquet',
+		'url':'https://learnodo-newtonic.com/wp-content/uploads/2013/07/The-Floor-Scrapers-by-Gustave-Caillebotte.jpeg'},
+		{'title': 'Absinthe',
+		'url':'https://learnodo-newtonic.com/wp-content/uploads/2013/07/The-Absinthe-Drinker-by-Edgar-Degas.jpg'},
+		{'title': 'Pont Boieldieu in Rouen, Rainy Weather',
+		'url':'https://learnodo-newtonic.com/wp-content/uploads/2013/07/Pont-Boieldieu-in-Rouen-Rainy-Weather-by-Camille-Pissarro.jpg'} ]
+		
+	photo_pages = [
+		{'title': 'Modern dykes, windmills and highways in the Netherlands',
+		'url':'https://files.brightside.me/files/news/part_4/46555/208855-3432610-1000-1446444849comment_pngkZ7y3otB9J0WJmTBEYYcmM3S6Tydz-1000-6d51e333c8-1484729696.jpg', 'views': 27 },
+		{'title': 'A temple covered in ash from the Ontake volcanic eruption, Japan',
+		'url':'https://brightside.me/article/100-best-photographs-without-photoshop-46555/#image1666460', 'views': 135},
+		{'title': 'Two worlds divided, New York, USA',
+		'url':'https://brightside.me/article/100-best-photographs-without-photoshop-46555/#image1666610', 'views': 400 } ]
+		
+	artwork = {'Abstract': {'pages': abstract_pages, 'views': 150, 'likes': 112 },
+			   'Still Life': {'pages': still_life_pages, 'views': 10, 'likes': 3 },
+			   'Impressionist': {'pages': impressionist_pages, 'views': 253, 'likes': 76 },
+			   'Photos': {'pages': photo_pages, 'views': 423, 'likes': 290 }, 
+			   
+			   }
+		
+	for art, art_data in artwork.items():
+		c = add_art(art)
+		for p in art_data["pages"]:
+			add_page(c, p["title"], p["url"])
+    
+	for c in Category.objects.all():
+		for p in Page.objects.filter(category=c):
+			print("- {0} - {1}".format(str(c), str(p)))
+
+def add_page(art, title, url, views=0):
+    p = Page.objects.get_or_create(category=art, title=title)[0]
+    p.url=url
+    p.views=views
+    p.save()
+    return p
+
+def add_art(name, views=0, likes=0):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.views=views
+    c.likes=likes
+    c.save()
+    return c
+
+if __name__ == '__main__':
+    print("Starting Artbud population script...")
+    populate()
