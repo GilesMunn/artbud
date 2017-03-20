252f7ff67b90545945c6650cf826243b6715dcb6
GilesGlasgow <2227780m@student.gla.ac.uk>
2017-03-20 20:34:13 +0000

    Started improving population script

--- artbud_project/population_script.py
+++ artbud_project/population_script.py
@@ -8,44 +8,47 @@ from artbud.models import Category, Page
 
 def populate():
 	
-	abstract_pages = [
-		{'title': 'Ideas',
-		'url':'http://inspirationseek.com/wp-content/uploads/2014/06/Abstract-Painting-Ideas.jpg', 'views': 12},
-		{'title': 'Nature',
-		'url':'http://inspirationseek.com/wp-content/uploads/2014/06/Abstract-Painting-Nature.jpg', 'views': 34},
-		{'title': 'Flower Nature',
-		'url':'http://inspirationseek.com/wp-content/uploads/2014/06/Abstract-Painting-Flower-Nature.jpg', 'views': 15} ]
+	photography = [
+		{title: 'Car in Damascus',user: 'Jack96', picture: '/images/photography/car.jpg', price: '£10', comment: 'A car'},
+		{title: 'Theatre Performer',user: '-dsfs-', picture: '/images/photography/dancer.jpg', price: '£210', comment: 'A dancer'},
+		{title: 'Green Tree Python',user: 'Giles123', picture: '/images/photography/python.jpg', price: '£33', comment: 'A snake'},
+		{title: 'Tsunami',user: '87-Dom', picture: '/images/photography/tsunami.jpg', price: '£52', comment: 'A classroom after a tsunami'},
+		{title: 'Giant Wave',user: 'JOHN_1', picture: '/images/photography/water.jpg', price: '£325', comment: 'A wave from a ship'}]
 		
-	still_life_pages = [
-		{'title': 'Bas Meeuws',
-		'url':'http://cdn.mos.cms.futurecdn.net/rmnxHaXkhmZjncyzMahdJU-650-80.jpg', 'views': 24 },
-		{'title': 'Elena K',
-		'url':'http://cdn.mos.cms.futurecdn.net/fff6c24beaf409b2d24e479a414374a9-650-80.jpg', 'views': 86 },
-		{'title': 'Joan Gamell',
-		'url':'http://cdn.mos.cms.futurecdn.net/6133a6789a940f061a2a4eaf898ad67d-650-80.jpg', 'views': 201 } ]
+	drawings_images = [
+		{title: 'Audi R8',user: '__Leah__', picture: '/images/drawings/audi.jpg', price: '£120', comment: 'An Audi R8'},
+		{title: 'Clint Eastwood',user: 'xx-Harry-xx', picture: '/images/drawings/clint.jpg', price: '£345', comment: 'Clint Eastwood portrait'},
+		{title: 'Rose',user: '12456789', picture: '/images/drawings/rose.jpg', price: '£36', comment: 'A white rose'},
+		{title: 'Big Ben',user: 'TomSmith', picture: '/images/drawings/big_ben.jpg', price: '£6', comment: 'Big Ben tower'},
+		{title: 'Tiger',user: 'Robotman', picture: '/images/drawings/tiger.jpg', price: '£35', comment: 'A friendly tiger'}]
 		
-	impressionist_pages = [
-		{'title': 'Les raboteurs de parquet',
-		'url':'https://learnodo-newtonic.com/wp-content/uploads/2013/07/The-Floor-Scrapers-by-Gustave-Caillebotte.jpeg'},
-		{'title': 'Absinthe',
-		'url':'https://learnodo-newtonic.com/wp-content/uploads/2013/07/The-Absinthe-Drinker-by-Edgar-Degas.jpg'},
-		{'title': 'Pont Boieldieu in Rouen, Rainy Weather',
-		'url':'https://learnodo-newtonic.com/wp-content/uploads/2013/07/Pont-Boieldieu-in-Rouen-Rainy-Weather-by-Camille-Pissarro.jpg'} ]
+	paintings_images = [
+		{title: 'Adam',user: 'Aida66', picture: '/images/paintings/adam.jpg', price: '£12', comment: 'The creation of man'},
+		{title: 'Persistence of Memory',user: 'LeoFitz', picture: '/images/paintings/memory.jpg', price: '£22', comment: 'Salvador Dali painting'},
+		{title: 'Girl With Pearl Earrings',user: 'AgntSimmons', picture: '/images/paintings/pearl_earring.jpg', price: '£47', comment: 'A pretty girl'},
+		{title: 'The Scream',user: 'Kyle-X', picture: '/images/paintings/scream.jpg', price: '£529', comment: 'A terrible figure screaming'},
+		{title: 'A Starry Night',user: 'Y_Jessie', picture: '/images/paintings/starry_night.jpg', price: '£222', comment: 'Van Gogh painting'}]
+	
+	sculpture_images = [
+		{title: 'Endless Column',user: 'TheDocW', picture: '/images/sculpture/column.jpg', price: '£5', comment: 'A large structure'},
+		{title: 'Expansion',user: 'Gilbs96', picture: '/images/sculpture/expansion.jpg', price: '£53', comment: 'New York sculpture'},
+		{title: "Michaelangelo's David",user: 'RoseT2005', picture: '/images/sculpture/david.jpg', price: '£324', comment: 'The most famous statue'},
+		{title: 'Running Mustangs',user: 'Quantum', picture: '/images/sculpture/mustangs.jpg', price: '£99', comment: 'Horses running sculpture'},
+		{title: "Michaelangelo's Pieta",user: 'PhilC', picture: '/images/sculpture/pieta.jpg', price: '£1', comment: 'Another famous statue'}]
 		
-	photo_pages = [
-		{'title': 'Modern dykes, windmills and highways in the Netherlands',
-		'url':'https://files.brightside.me/files/news/part_4/46555/208855-3432610-1000-1446444849comment_pngkZ7y3otB9J0WJmTBEYYcmM3S6Tydz-1000-6d51e333c8-1484729696.jpg', 'views': 27 },
-		{'title': 'A temple covered in ash from the Ontake volcanic eruption, Japan',
-		'url':'https://files.brightside.me/files/news/part_4/46555/209105-3446910-1000-144645398520141015-ontake-1000-66810123c4-1484729696.jpg', 'views': 135},
-		{'title': 'Two worlds divided, New York, USA',
-		'url':'https://files.brightside.me/files/news/part_4/46555/209955-d-v-a-a-1000-1446452908-1000-6e726f73ab-1484729696.jpg', 'views': 400 } ]
+	other_images = [
+		{title: 'My Bed',user: 'Skye12', picture: '/images/other/bed.jpg', price: '£567', comment: 'Just a bed'},
+		{title: 'Easter Island Heads',user: '64Android', picture: '/images/other/easter_island.jpg', price: '£3452', comment: 'Those famous heads'},
+		{title: 'Fish Tank',user: 'Gauntlets', picture: '/images/other/fish_tank.jpg', price: '£54', comment: 'Abstract art of a fish tank'},
+		{title: 'Video Game',user: 'Art_Lover', picture: '/images/other/game.jpg', price: '£14', comment: 'Digital art'},
+		{title: 'Waterfall',user: 'HiEverybody', picture: '/images/other/waterfall.jpg', price: '£678', comment: 'Strange 3D drawing'}]
+	
 		
-	artwork = {'Abstract': {'pages': abstract_pages, 'views': 150, 'likes': 112 },
-			   'Still Life': {'pages': still_life_pages, 'views': 10, 'likes': 3 },
-			   'Impressionist': {'pages': impressionist_pages, 'views': 253, 'likes': 76 },
-			   'Photos': {'pages': photo_pages, 'views': 423, 'likes': 290 }, 
-			   
-			   }
+	artwork = {"Photography": {"info": photography_images},
+			   "Drawing": {"info": drawings_images},
+			   "Sculpture": {"info": sculpture_images},
+			   "Other": {"info": other_images},
+			   "Painting": {"info": painting_images} }
 		
 	for art, art_data in artwork.items():
 		c = add_art(art)
